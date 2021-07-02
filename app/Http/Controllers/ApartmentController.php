1b3b3bc435f6e7d1b2d437064eafa4c430d3d9dc
giang <70989443+giang2210@users.noreply.github.com>
2021-07-02 12:26:02 +0700

    update

---
+++ app/Http/Controllers/ApartmentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Apartment;
+use Illuminate\Http\Request;
+
+class ApartmentController extends Controller
+{
+    public function listApartment(){
+        $apartments = Apartment::paginate(6);
+        return view("list",[
+            "apartments"=>$apartments
+        ]);
+    }
+}
